e4a56a731a6fc6618b88d2f2c457f44e7379d3cc
mag.cats387 <mag.cats387@gmail.com>
2022-03-05 14:59:07 +0900

    #modify Organizing the Code

--- main.py
+++ main.py
@@ -3,20 +3,19 @@
 
 # TODO 例外処理実装
 
-from lib2to3.pytree import convert
 import about
+import csv
 import detail
+import pprint
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
 from selenium.webdriver.chrome import service as ch
 
-import time
-
-import csv
-import pprint
-
+#
+# ＊メインメソッド＊
+#
 def main():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
@@ -29,12 +28,129 @@ def main():
     link_list = fetchAbout(driver, 'https://opensea.io/mag_387')
     print("取得可能なリンク数は" + str(len(link_list)) + "です")
 
-    dts = getDetails(driver, link_list, 10000)
+    dts = getDetails(driver, link_list, 1)
     
     # 書き出し
     arr = getDetailsArray(dts)
     writeCsv(arr)
 
+
+#
+# ＊メインロジック＊
+#
+
+
+# 一覧情報を取得
+# TODO Aboutを別の用語に置き換える
+def fetchAbout(driver, url):
+    driver.get(url)
+    time.sleep(1)
+
+    # 縮小ボタンを選択
+    # small_btn = driver.find_element(by=By.XPATH, value='//button[contains(@class,"bnWGYU")]')
+    if (small_btn := findElements(driver, '//button[contains(@class,"bnWGYU")]')) != False:
+        small_btn.click()
+    time.sleep(0.5)
+    # 表示倍率を縮小
+    driver.execute_script("document.body.style.zoom='20%'")
+    time.sleep(0.5)
+    # ページを一番下までスクロール
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # 読み込み待ち
+    time.sleep(8)
+
+    # 詳細ページのリンクを取得
+    if (assets_el := findElements(driver, '//div[contains(@class,"AssetsSearchView--assets")]/div/div/div/div/div/article/a', True)) != False:
+        about_list = []
+        for s in assets_el:
+            about_itm = about.About(detail_url = s.get_attribute("href"))
+            about_list.append(about_itm)
+        return about_list
+    return False
+
+# 詳細情報を上限付きで取得する
+def getDetails(driver, about_list = [], limit = 100000):
+    count = 0
+    detail_list = []
+    for about in about_list:
+        if count == limit:
+            break
+        count += 1
+        print("こちらのNFTの情報を取得します:" + about.detail_url)
+        detail_list.append(fetchDetail(driver, about.detail_url))
+        print("進捗:" + str(count) + "/" + str(len(about_list)))
+    return detail_list
+
+# 詳細情報を取得
+def fetchDetail(driver, url):
+    detail_data = detail.Detail()
+
+    driver.get(url)
+    time.sleep(1)
+
+    # 表示倍率を縮小
+    driver.execute_script("document.body.style.zoom='60%'")
+    # 読み込み待ち
+    time.sleep(2)
+
+    # コレクション名
+    detail_data.collection_name = assignElementValue(driver,\
+        '//div[contains(@class,"item--collection-detail")]/div/a', 'text')
+
+    # NFT名
+    detail_data.name = assignElementValue(driver, \
+        '//section[@class="item--header"]/h1', 'text')
+
+    # NFT所有者ブロック
+    if (owner_itm := findElements(driver, '//section[@class="item--counts"]/div/div/a')) != False:
+        # NFTの所有者名（１つの場合のみ）
+        detail_data.owner_name = owner_itm.text
+        # 所有者URL（１つの場合のみ）
+        detail_data.owner_url = owner_itm.get_attribute("href")
+
+    if (creator_name := findElements(driver, \
+        '//div[contains(@id,"Body react-aria-")]/div/div/section/div/a/span')) != False:
+        # 作成者の名前
+        detail_data.creator_name = creator_name.text
+        # 作成者のURL
+        detail_data.creator_address = assignElementValue(driver,\
+            '//div[contains(@id,"Body react-aria-")]/div/div/section/div/a', 'href')
+
+    # 説明
+    # TODO:改行処理の実装
+    # description = driver.find_element_by_xpath('//div[contains(@class,"item--description-text")]/p').text
+
+    # Detailsを開く
+    if (detail_btn := findElements(driver,\
+        '//button[span[contains(text(),"Details")]]')) != False:
+        driver.execute_script("arguments[0].click();", detail_btn)
+        time.sleep(0.1)
+
+    # NFTのコントラクトアドレス
+    if (scan_contract_address := assignElementValue(driver,\
+        '//span[contains(@class, "elqhCm cCfKUE jmAsQO")]/a', 'href')) != "":
+        detail_data.contract_address = scan_contract_address.split('/')[-1]
+
+    # トークンID
+    # token_id = driver.find_element_by_xpath('//div[@class="Blockreact__Block-sc-1xf18x6-0 elqhCm")]/div/span').get_attribute("href")
+
+    # コンテンツを選択する
+    if (content_btn := findElements(driver,\
+            '//div[@class="item--summary"]/article/div/div/div/div/img')) != False:
+        driver.execute_script("arguments[0].click();", content_btn)
+        time.sleep(0.1)
+        # コンテンツの中身のURL
+        detail_data.data_url = assignElementValue(driver, '//div[@class="Overlayreact__Overlay-sc-1yn7g51-0 ebMEfa"]/div/div/div/div/div/img', 'src')
+
+    return detail_data
+
+#
+# ＊中間処理用メソッド＊
+#
+
+# データ処理系
+
+# csv書き込み時に使用するデータ配列を作成
 def getDetailsArray(details):
     pprint.pprint(details)
     arr = [["NFT name",
@@ -75,108 +191,34 @@ def writeCsv(array):
     with open('./sample.csv') as f:
         print(f.read())
 
-# 一覧情報を取得
-# TODO Aboutを別の用語に置き換える
-def fetchAbout(driver, url):
-    driver.get(url)
-    time.sleep(1)
 
-    # 縮小ボタンを選択
-    small_btn = driver.find_element(by=By.XPATH, value='//button[contains(@class,"bnWGYU")]')
-    small_btn.click()
-    time.sleep(0.5)
-    # 表示倍率を縮小
-    driver.execute_script("document.body.style.zoom='20%'")
-    time.sleep(0.5)
-    # ページを一番下までスクロール
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    # 読み込み待ち
-    time.sleep(8)
+# Selenium補助系
 
-    # 詳細ページのリンクを取得
-    assets_el = driver.find_elements(by=By.XPATH, value='//div[contains(@class,"AssetsSearchView--assets")]/div/div/div/div/div/article/a')
-    # assets_count = len(assets_el)
+# 要素を取得し、指定した変数に代入する　単数の要素に限る
+#type: text, href
+def assignElementValue(driver, xpath, type="text"):
+    if xpath == "":
+        return ""
+    element = findElements(driver, xpath)
+    if element == False:
+        return ""
+    
+    if type == "text":
+        return element.text    
+    elif type == "href":
+        return element.get_attribute("href")
+    elif type == "src":
+        return element.get_attribute("src")
+    return ""
 
-    about_list = []
-    for s in assets_el:
-        about_itm = about.About(detail_url = s.get_attribute("href"))
-        about_list.append(about_itm)
-
-    # 取得結果を表示
-    # return [s.get_attribute("href") for s in assets_el]
-    return about_list
-
-# 詳細情報を上限付きで取得する
-def getDetails(driver, about_list = [], limit = 100000):
-    count = 1
-    detail_list = []
-    for about in about_list:
-        if count == limit:
-            break
-        count += 1
-        print("こちらのNFTの情報を取得します:" + about.detail_url)
-        detail_list.append(fetchDetail(driver, about.detail_url))
-        print("進捗:" + str(count) + "/" + str(len(about_list)))
-    return detail_list
-
-# 詳細情報を取得
-def fetchDetail(driver, url):
-    detail_data = detail.Detail()
-
-    driver.get(url)
-    time.sleep(1)
-
-    # 表示倍率を縮小
-    driver.execute_script("document.body.style.zoom='60%'")
-    # 読み込み待ち
-    time.sleep(2)
-
-    # コレクション名
-    detail_data.collection_name = driver.find_element(by=By.XPATH, value='//div[contains(@class,"item--collection-detail")]/div/a').text
-    # NFT名
-    detail_data.name = driver.find_element(by=By.XPATH, value='//section[@class="item--header"]/h1').text
-
-    # NFT所有者ブロック
-    owner_itm = driver.find_elements(by=By.XPATH, value='//section[@class="item--counts"]/div/div/a')
-    if len(owner_itm) > 0 :
-        # NFTの所有者名（１つの場合のみ）
-        detail_data.owner_name = owner_itm[0].text
-        # 所有者URL（１つの場合のみ）
-        detail_data.owner_url = owner_itm[0].get_attribute("href")
-
-    creator_name = driver.find_elements(by=By.XPATH, value='//div[contains(@id,"Body react-aria-")]/div/div/section/div/a/span')
-    if len(creator_name) > 0:
-        # 作成者の名前
-        detail_data.creator_name = creator_name[0].text
-        # 作成者のURL
-        detail_data.creator_address = driver.find_element(by=By.XPATH, value='//div[contains(@id,"Body react-aria-")]/div/div/section/div/a').get_attribute("href")
-
-    # 説明
-    # TODO:改行処理の実装
-    # description = driver.find_element_by_xpath('//div[contains(@class,"item--description-text")]/p').text
-
-    # Detailsを開く
-    detail_btn = driver.find_element(by=By.XPATH, value='//button[span[contains(text(),"Details")]]')
-    driver.execute_script("arguments[0].click();", detail_btn)
-    time.sleep(0.1)
-
-    # NFTのコントラクトアドレス
-    scan_contract_address = driver.find_element(by=By.XPATH, value='//span[contains(@class, "elqhCm cCfKUE jmAsQO")]/a').get_attribute("href")
-    if scan_contract_address != "":
-        detail_data.contract_address = \
-            scan_contract_address.split('/')[-1]
-    # トークンID
-    # token_id = driver.find_element_by_xpath('//div[@class="Blockreact__Block-sc-1xf18x6-0 elqhCm")]/div/span').get_attribute("href")
-
-    # コンテンツを選択する
-    content_btn = driver.find_elements(by=By.XPATH, value='//div[@class="item--summary"]/article/div/div/div/div/img')
-    if len(content_btn) > 0:
-        driver.execute_script("arguments[0].click();", content_btn[0])
-        time.sleep(0.1)
-        # コンテンツの中身のURL
-        detail_data.data_url = driver.find_element(by=By.XPATH, value='//div[@class="Overlayreact__Overlay-sc-1yn7g51-0 ebMEfa"]/div/div/div/div/div/img').get_attribute("src")
-
-    return detail_data
+# 単数または複数の要素を取得する
+def findElements(driver, xpath, multi=False):
+    elements = driver.find_elements(by=By.XPATH, value=xpath)
+    if len(elements) != 0:
+        if multi:
+            return elements
+        return elements[0]
+    return False
 
 if __name__ == "__main__":
     main()
